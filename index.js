544f099e3f8dddd27f4af2816f8015bf4f540338
seolbinlee <seolbinlee@JHs-iMac.local>
2020-10-30 18:01:20 +0900

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 // DB setting
@@ -22,36 +23,66 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 
 // DB schema
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
+var navifeedSchema = mongoose.Schema({
+  // name:{type:String, required:true, unique:true},
+  content:{type: String, required: true, unique: true},
+  // email:{type:String},
+  // phone:{type:String}
 });
-var Contact = mongoose.model('contact', contactSchema);
+var Navifeed = mongoose.model('navifeed', navifeedSchema);
 
 // Routes
 // Home
 app.get('/', function(req, res){
-  res.redirect('/contacts');
+  res.redirect('/navifeeds');
 });
-// Contacts - Index
-app.get('/contacts', function(req, res){
-  Contact.find({}, function(err, contacts){
+// Navifeeds - Index
+app.get('/navifeeds/', function(req, res){
+  Navifeed.find({}, function(err, navifeeds){
     if(err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
+    res.render('navifeeds/index', {navifeeds:navifeeds});
   });
 });
-// Contacts - New
-app.get('/contacts/new', function(req, res){
-  res.render('contacts/new');
+// Navifeeds - New
+app.get('/navifeeds/new', function(req, res){
+  res.render('navifeeds/new');
 });
-// Contacts - create
-app.post('/contacts', function(req, res){
-  Contact.create(req.body, function(err, contact){
+// Navifeeds - create
+app.post('/navifeeds', function(req, res){
+  Navifeed.create(req.body, function(err, navifeed){
     if(err) return res.json(err);
-    res.redirect('/contacts');
+    res.redirect('/navifeeds');
+  });
+});
+// Navifeeds - show
+app.get('/navifeeds/:id', function(req, res){
+  Navifeed.findOne({_id:req.params.id}, function(err, navifeed){
+    if(err) return res.json(err);
+    res.render('navifeeds/show', {navifeed:navifeed});
+  });
+});
+// Navifeeds - edit
+app.get('/navifeeds/:id/edit', function(req, res){
+  Navifeed.findOne({_id:req.params.id}, function(err, navifeed){
+    if(err) return res.json(err);
+    res.render('navifeeds/edit', {navifeed:navifeed});
+  });
+});
+// Navifeeds - update
+app.put('/navifeeds/:id', function(req, res){
+  Navifeed.findOneAndUpdate({_id:req.params.id}, req.body, function(err, navifeed){
+    if(err) return res.json(err);
+    res.redirect('/navifeeds/'+req.params.id);
+  });
+});
+// Navifeeds - destroy
+app.delete('/navifeeds/:id', function(req, res){
+  Navifeed.deleteOne({_id:req.params.id}, function(err){
+    if(err) return res.json(err);
+    res.redirect('/navifeeds');
   });
 });
 
